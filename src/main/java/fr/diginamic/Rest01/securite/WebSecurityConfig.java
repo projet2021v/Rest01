64426faa3394f92c80ea55c8f06cd0c6169d2017
Vincent LB <vincent.lebras2000@campus-eni.fr>
2021-10-25 09:18:06 +0200

    logout link added

--- src/main/java/fr/diginamic/Rest01/securite/WebSecurityConfig.java
+++ src/main/java/fr/diginamic/Rest01/securite/WebSecurityConfig.java
@@ -6,14 +6,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http
+//		.logout();
+//	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
